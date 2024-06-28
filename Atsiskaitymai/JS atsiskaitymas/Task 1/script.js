ef76baca34344548d89cb8e6b4b6ff03fd5bae77
HierComsDatBoi <dainius.slekonis@gmail.com>
2024-06-28 14:22:25 +0300

    paturbinimai

--- Atsiskaitymai/JS atsiskaitymas/Task 1/script.js
+++ Atsiskaitymai/JS atsiskaitymas/Task 1/script.js
@@ -8,31 +8,37 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
-
 document.querySelector('.wrapper > form')
-.addEventListener('submit', e =>{
-  e.preventDefault();
-  
-  const kilogramai = e.target.elements.search.value;
-  console.log(kilogramai);
+  .addEventListener('submit', e => {
+    e.preventDefault();
 
-  const svarai = kilogramai * 2.2046;
-  const gramai = kilogramai / 0.0010000;
-  const uncijos = kilogramai * 35.274;
-  
+    const kg = e.target.elements.search.value;
+    output = document.querySelector('#output');
+    output.classList.add('white');
+    output.textContent = '';
+    //patikrinama ar ivestas skaicius ir ne tuscia reiksme
 
-// atsakymu atvaizdavimas
-  output = document.querySelector('#output');
+    if (isNaN(kg) || kg == '') {
+      output.textContent = 'Error: Not a number';
+    } else {
 
-  const parSvarai = document.createElement('p');
-  parSvarai.textContent = `${svarai} lb`;
 
-  const parGramai = document.createElement('p');
-  parGramai.textContent = `${gramai} g`;
+      const lb = kg * 2.2046;
+      const gr = kg / 0.0010000;
+      const oz = kg * 35.274;
 
-  const parUncijos = document.createElement('p');
-  parUncijos.textContent = `${uncijos} oz`;
+      // atsakymu atvaizdavimas
 
-  output.append(parSvarai,parGramai,parUncijos);
-  e.target.reset();
-});
+      const parLb = document.createElement('p');
+      parLb.textContent = `${kg} kg = ${lb} lb`;
+
+      const parGr = document.createElement('p');
+      parGr.textContent = `${kg} kg = ${gr} g`;
+
+      const parOz = document.createElement('p');
+      parOz.textContent = `${kg} kg = ${oz} oz`;
+
+      output.append(parLb, parGr, parOz);
+    }
+    e.target.reset();
+  });
