4ab8cf69032323594f9f914f0a2fe416344cb434
HierComsDatBoi <dainius.slekonis@gmail.com>
2024-07-19 10:01:14 +0300

    Uzduotis 4 padaryta

--- Atsiskaitymai/TS atsiskaitymas/Task 4/script.js
+++ Atsiskaitymai/TS atsiskaitymas/Task 4/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /* ------------------------------ TASK 4 -----------------------------------
 Parašykite TS funkciją, kuri priima tekstą ir grąžina skaičių susidedantį iš vienetų ir nulių tokio ilgio, kokio yra pats žodis. Skaičius visada prasideda vienetu.
 
@@ -6,20 +7,18 @@ Pvz.:
   "kebabas" --> 1010101
   "a"       --> 1
 -------------------------------------------------------------------------- */
-
 // let word:string = 'Dainius';
-
-function zeroOne(word:string):number[] {
-  let array:number[] = [];
-  for (let i = 0; i < word.length; i++) {
-      if (i == 0 || i % 2 != 1) {
-          array.push(1);
-      }
-      else {
-          array.push(0);
-      }
-  }
-  return array;
-};
-
+function zeroOne(word) {
+    let array = [];
+    for (let i = 0; i < word.length; i++) {
+        if (i == 0 || i % 2 != 1) {
+            array.push(1);
+        }
+        else {
+            array.push(0);
+        }
+    }
+    return array;
+}
+;
 console.log(zeroOne('Dainius'));
